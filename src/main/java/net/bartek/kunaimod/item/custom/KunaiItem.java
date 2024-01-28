0c575bbd74f122140eddd9a5f90e1203b9e3e958
FrozeyNight <125805827+FrozeyNight@users.noreply.github.com>
2024-01-28 21:36:15 +0100

    Update KunaiItem.java

--- src/main/java/net/bartek/kunaimod/item/custom/KunaiItem.java
+++ src/main/java/net/bartek/kunaimod/item/custom/KunaiItem.java
@@ -95,21 +95,23 @@ public class KunaiItem extends SwordItem {
         Vec3 daggerPos = lastThrownKunai.position();
 
         if (player instanceof ServerPlayer) {
-            ServerPlayer serverPlayer = (ServerPlayer) player;
 
             // Check for nearby entities and make them leap into the air
             List<LivingEntity> nearbyEntities = world.getEntitiesOfClass(LivingEntity.class,
-                    new AABB(daggerPos.x() - 10.0, daggerPos.y() - 10.0, daggerPos.z() - 10.0,
-                            daggerPos.x() + 10.0, daggerPos.y() + 10.0, daggerPos.z() + 10.0));
+                    new AABB(daggerPos.x() - 5.0, daggerPos.y() - 5.0, daggerPos.z() - 5.0,
+                            daggerPos.x() + 5.0, daggerPos.y() + 5.0, daggerPos.z() + 5.0));
 
             for (LivingEntity entity : nearbyEntities) {
                 // Calculate direction vector from entity to daggerPos
                 Vec3 direction = daggerPos.subtract(entity.position()).normalize();
 
                 // Apply a force towards daggerPos using the direction vector
-                double strength = 2;  // Adjust this value to control the strength of the gravity pull
-                entity.setDeltaMovement(entity.getDeltaMovement().add(direction.x * strength, direction.y * strength, direction.z * strength));
+                double strength = 1;  // Adjust this value to control the strength of the gravity pull
+                entity.setDeltaMovement(direction.x * strength, direction.y * strength, direction.z * strength);
+
+                entity.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 40, 255, false, false));
             }
+
 
             lastThrownKunai.kunaiItem.hurtAndBreak(16, player, p -> p.broadcastBreakEvent(player.getUsedItemHand()));
 
@@ -118,6 +120,35 @@ public class KunaiItem extends SwordItem {
             playerKunais.remove(playerKunais.size() - 1);
 
             world.playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.GLASS_BREAK, SoundSource.PLAYERS, 1f , 1f);
+
+        }
+    }
+
+    public static void BlindingKunai(Player player, Level world){
+        ThrownKunai lastThrownKunai = findLastThrownKunai(player);
+
+        Vec3 daggerPos = lastThrownKunai.position();
+
+        if (player instanceof ServerPlayer) {
+
+            // Check for nearby entities and make them leap into the air
+            List<LivingEntity> nearbyEntities = world.getEntitiesOfClass(LivingEntity.class,
+                    new AABB(daggerPos.x() - 5.0, daggerPos.y() - 5.0, daggerPos.z() - 5.0,
+                            daggerPos.x() + 5.0, daggerPos.y() + 5.0, daggerPos.z() + 5.0));
+
+            for (LivingEntity entity : nearbyEntities) {
+                entity.addEffect(new MobEffectInstance(MobEffects.BLINDNESS, 200, 255));
+            }
+
+
+            lastThrownKunai.kunaiItem.hurtAndBreak(16, player, p -> p.broadcastBreakEvent(player.getUsedItemHand()));
+
+            // Remove the last thrown kunai from the list
+            List<ThrownKunai> playerKunais = KunaiTracking.thrownKunaiMap.get(player);
+            playerKunais.remove(playerKunais.size() - 1);
+
+            world.playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.WARDEN_NEARBY_CLOSER, SoundSource.PLAYERS, 2f , 1f);
+
         }
     }
 
